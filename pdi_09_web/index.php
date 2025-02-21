9cf7bc7bc4a1cad0c0d4cbacdf84e152d9c1b287
Augustin-Pierre <168812338+Augustin-Pierre@users.noreply.github.com>
2025-02-21 10:46:04 +0100

    Add files via upload

---
+++ pdi_09_web/index.php
@@ -0,0 +1,33 @@
+<?php
+require 'flight/Flight.php';
+
+$link =   pg_connect("host=localhost port=5432 dbname=PDI09 user=postgres password=postgres");
+Flight::set('db', $link);
+
+Flight::route('/', function() {
+    Flight::render('accueil'); 
+});
+
+Flight::route('/api/fichiers', function() {  
+    $db = Flight::get('db');
+    $table = $_GET["t"];
+    if (isset($_GET['id']) && !empty($_GET['id'])) {
+        $reponse = pg_query($db,"SELECT ST_AsGeoJson(geom) AS geom, fid FROM " .$table." where fid = '". $_GET['id']."'");
+        $resultats = pg_fetch_all($reponse, PGSQL_ASSOC)[0];
+    }
+
+    else{
+        $reponse = pg_query($db, "SELECT ST_AsGeoJson(geom) AS geom, fid FROM " .$table);
+        $resultats = pg_fetch_all($reponse, PGSQL_ASSOC);
+        
+    }
+
+    Flight::json(['geom' => $resultats]);
+});
+
+
+
+
+Flight::start();
+
+?>
---
+++ pdi_09_web/views/accueil.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>PDI09</title>
+        <link rel="stylesheet" href="/assets/style.css">
+        <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css">
+        <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/vue"></script>
+    </head>
+
+    <body>
+
+        <header>
+        <img src="/images/pnmgl.png" alt="Logo du Parc Naturel Marin du Golfe du Lion">
+        <p>Parc Naturel Marin du Golfe du Lion</p>
+        <P>SITE DE VISUALISATION CARTOGRAPHIQUE</P>
+        <img src="/images/OFB.png" alt="Logo de l'Office Français de la Biodiversité">
+        <img src="/images/Republique_Francaise_RVB.png" alt="Logo de la République Française">
+        </header>
+
+        <div id = "map"></div>
+
+        <div id = "app">
+
+        <div id = "selection">
+            <h1>Données</h1>
+            <label>Perpignan<input v-model="perpignan" @change="cocher" type="checkbox" name="perpignan" ></label>
+            <label>Habitats d'intérêt<input v-model="habitat" @change="cocher2" type="checkbox" name="habitat" ></label>
+            <label>Données éco ?<input v-model="eco" @change="cocher3" type="checkbox" name="eco" ></label>
+        </div>
+        
+        </div>
+        
+                
+      
+        <script src="/assets/javascript.js"></script>  
+         
+    </body>    
+
+</html>
